14282f4a992a55e0635da1b2ce8850a13387fec0
CzerniakJ <59015400+CzerniakJ@users.noreply.github.com>
2020-04-15 11:00:55 +0200

    /

--- lista5/zad8.py
+++ lista5/zad8.py
@@ -1,15 +1,16 @@
 class samogloski:
     def __init__(self, data):
         self.data = data
-        self.index = len(data)
+        self.index = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.index == 0:
+        if self.index == len(self.data):
             raise StopIteration
-        elif self.data[self.index] == ("a" or "ą" or "e" or "ę" or "i" or "o" or "u" or "y"):
+        self.index += 1
+        if (self.data[self.index] == ("a" or "e" or  "i" or "o" or "u" or "y")):
             return self.data[self.index]
-        self.index = self.index - 1
+
 print([a for a in samogloski("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat.")])       
